35fb5704cec79321ce5105a7802d3971a5b6037c
innthomas <innthomas00158@gmail.com>
2022-04-12 23:23:05 +0100

    form edits

--- src/App.js
+++ src/App.js
@@ -1,86 +1,107 @@
-import React , {useReducer, useState } from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
-const formReducer = (state, event) => {
-  if(event.reset) {
-    return {
-      apple: '',
-      count: 0,
-      name: '',
-      'gift-wrap': false,
-    }
-  }
-  return {
-    ...state,
-    [event.name]: event.value
-  }
- }
-
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, { count: 100,});
-  const [submitting, setSubmitting] = useState(false);
-  const handleSubmit = event => {
-    event.preventDefault();
-    setSubmitting(true);
-
-   setTimeout(() => {
-     setSubmitting(false);
-     setFormData({
-      reset: true
+  const [state, setState] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    address: "",
+    gender: "",
+    message: "",
+  })
+  const handleChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
     })
-   }, 3000)
   }
 
-  const handleChange = event => {
-    const isCheckbox = event.target.type === 'checkbox';
-    setFormData({
-      name: event.target.name,
-      value: isCheckbox ? event.target.checked : event.target.value,
-    });
-  }
   return (
-    <div className="wrapper">
-      <h1>How About Them Apples</h1>
-      {submitting &&
-       <div>
-         You are submitting the following:
-         <ul>
-           {Object.entries(formData).map(([name, value]) => (
-             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
-           ))}
-         </ul>
-       </div>
-     }
-      <form onSubmit={handleSubmit}>
-      <fieldset disabled={submitting}>
-         <label>
-           <p>Name</p>
-           <input name="name" onChange={handleChange} value={formData.name || ''}/>
-         </label>
-       </fieldset>
-       <fieldset disabled={submitting}>
-         <label>
-           <p>Apples</p>
-           <select name="apple" onChange={handleChange} value={formData.apple || ''}>
-               <option value="">--Please choose an option--</option>
-               <option value="fuji">Fuji</option>
-               <option value="jonathan">Jonathan</option>
-               <option value="honey-crisp">Honey Crisp</option>
-           </select>
-         </label>
-         <label>
-           <p>Count</p>
-           <input type="number" name="count" onChange={handleChange} step="1" value={formData.count || ''}/>
-         </label>
-         <label>
-           <p>Gift Wrap</p>
-           <input type="checkbox" name="gift-wrap" onChange={handleChange} checked={formData['gift-wrap'] || false} disabled={formData.apple !== 'fuji'} />
-         </label>
-       </fieldset>
-       <button type="submit" disabled={submitting}>Submit</button>
+    <div>
+      <div>
+
+      <h1>React Form Handling</h1>
+      <form>
+        <label>
+          First Name:{""} 
+          <input type="text" name="fname" value={state.fname} onChange={handleChange}/>
+        </label>{""}
+        <label>
+          Last Name:{" "}
+          <input
+            type="text"
+            name="lname"
+            value={state.lname}
+            onChange={handleChange}
+          />
+        </label>
+        <br /><br />
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          checked={state.gender === "male"}
+          onChange={handleChange}
+        />
+{" "}
+        Male
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          checked={state.gender === "female"}
+          onChange={handleChange}
+        />
+{" "}
+        Female
+      </label>
+      {/* email */}
+      <br/>
+      <label>
+          email:{" "}
+          <input
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={handleChange}
+          />
+        </label>
+        {/* address */}
+        <br/>
+      <label>
+          address:{" "}
+          <input
+            type="text"
+            name="address"
+            value={state.address}
+            onChange={handleChange}
+          />
+        </label>
+
+        <br />
+      <label>
+        Bio:{" "}
+        <textarea
+          name="message"
+          value={state.message}
+          onChange={handleChange}
+        />
+      </label>
       </form>
+      </div>
+      <div>
+      <h5>Name: {state.fname} {state.lname}</h5>
+      <h5>Gender Selected : {state.gender}</h5>
+      <h5>email: {state.email}</h5>
+      <h5>address: {state.address}</h5>
+      <p>Bio: {state.message}</p>
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
